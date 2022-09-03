985ef79d160bac94670f225027945a5dbbbd638f
Tsegawit Tadesse <106810498+shiroisanhe60@users.noreply.github.com>
2022-09-03 11:51:38 -0700

    Update auth.py

--- Project/03_coffee_shop_full_stack/starter_code/backend/src/auth/auth.py
+++ Project/03_coffee_shop_full_stack/starter_code/backend/src/auth/auth.py
@@ -5,7 +5,7 @@ from jose import jwt
 from urllib.request import urlopen
 
 
-AUTH0_DOMAIN = 'shiroisanhe-fsnd.eu.auth0.com'
+AUTH0_DOMAIN = 'dev-fchld9qn.us.auth0.com'
 ALGORITHMS = ['RS256']
 API_AUDIENCE = 'dev'
 
